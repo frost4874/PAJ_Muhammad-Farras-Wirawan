a477f363d2f7892d98e4bfe7a1fd78944bc174ac
Farras Wirawan <farraswirawan4874@gmail.com>
2024-02-02 09:44:42 +0700

    Minggu_1
    Co-Authored-By: arvitaaguskurniasari <157940264+arvitaaguskurniasari@users.noreply.github.com>

---
+++ Minggu_1/larafun/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/about', function () {
+    return 'Ini Halaman About';
+});
+
+Route::get('/servis', function () {
+    return view('services');
+});
