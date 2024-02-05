3349435a6f09742cdaa04e2c0a32ea3ada9eab31
Farras Wirawan <farraswirawan4874@gmail.com>
2024-02-05 17:24:31 +0700

    Minggu 2

---
+++ Minggu_2/larafun/resources/views/tu_uc.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Teacher User View</title>
+</head>
+<body>
+<h1>Ini adalah halaman Teacher User View pada User Controller</h1>
+</body>
+</html>
