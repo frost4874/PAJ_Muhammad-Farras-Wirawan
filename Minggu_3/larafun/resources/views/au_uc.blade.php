776f0414064f642be8c1fee5817fbc479222aaac
Farras Wirawan <farraswirawan4874@gmail.com>
2024-02-12 12:40:25 +0700

    Minggu 3

---
+++ Minggu_3/larafun/resources/views/au_uc.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Admin User View</title>
+</head>
+<body>
+<h1>Ini adalah halaman Admin User View pada User Controller</h1>
+</body>
+</html>
